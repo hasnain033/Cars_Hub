d80ae36a7f0041b8901845eebf9a301e64e9c83d
hasnain ali <eng.hasnain99@gmail.com>
2023-06-25 13:59:05 +0500

    manufacturer search dropdown solved

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -39,7 +39,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer}:SearchManufacturerPr
             leaveTo='opacity-0'
             afterLeave={() => setQuery("")} // Reset the search query after the transition completes
           >
-            <Combobox.Options className='absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+            <Combobox.Options className='absolute z-20 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
               static>
               {filterManufacturer.map((item)=>{
                 return (
